3aa891bcc1005d739608f76a7ef517c26e58768f
TonNom <tyjustmusic@gmail.com>
2025-03-04 10:36:51 +0000

    15 commits

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 /*import adapter from "svelte-adapter-bun";*/
 import adapter from '@sveltejs/adapter-vercel';
 
-
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
