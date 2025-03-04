26650fbb4f73e120b8aa50b93cf998e577544e40
TonNom <tyjustmusic@gmail.com>
2025-03-04 10:22:22 +0000

    10 commits

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,25 @@
-import adapter from "svelte-adapter-bun";
+import adapter from '@sveltejs/adapter-static';
+
+export default {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
+};
+//import adapter from "svelte-adapter-bun";
+/*import adapter from '@sveltejs/adapter-static';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+/*const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
@@ -16,3 +32,4 @@ const config = {
 };
 
 export default config;
+*/
